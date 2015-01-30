9e7452746b7954935df12e73eef5fd3a83fe0c85
sunny200745 <sunny.200745@gmail.com>
2015-01-30 16:37:58 +0530

    Adding image in progress to mix with style

--- app/scripts/controllers/page.js
+++ app/scripts/controllers/page.js
@@ -10,5 +10,11 @@
 angular.module('angNewsApp')
   .controller('PageCtrl', function ($scope) {
     $scope.moduleName = "Page";
-    console.debug("I am PAGE CONTROLLER !! EVERY ONE PARENT :)")
+    console.debug("I am PAGE CONTROLLER !! EVERY ONE PARENT :)");
+    	
+    	
+		setInterval(function(){ 
+		    $('#heading').toggleClass('magictime spaceInDown');
+		}, 1000 );
+    
   });
